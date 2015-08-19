e1d8c0ce90cf77f0d21890cac067c0170795b246
inuscript <inuscript@gmail.com>
2015-08-19 19:18:05 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 var glob = require("glob")
 module.exports = {
-  entries: glob.sync("./js/entries/**/*.js"),
+  entry: glob.sync("./js/entries/**/*.js"),
   output: {
     path: "./public/webpack/",
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|vendor\/assets)/,
+        loader: 'babel'
+      }
+    ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
